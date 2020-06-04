070e7601d6532efeb387b674bf583ab580c21f69
jhryniw <jhryniw@gmail.com>
2020-06-04 14:31:11 -0600

    colours

---
+++ src/constants/colours.js
@@ -0,0 +1,11 @@
+export const PRIMARY: "#0F9CFC";       // blue
+export const PRIMARY_DARK: "#0F25FC";  // dark blue
+export const PRIMARY_LIGHT: "#0FFCE5"; // light blue
+export const ACCENT: "#FCC332";        // orange
+export const OFF_WHITE: "#F0F1FC";
+
+const theme = {
+  PRIMARY, PRIMARY_DARK, PRIMARY_LIGHT, ACCENT, WHITE: OFF_WHITE
+};
+
+export default theme;
