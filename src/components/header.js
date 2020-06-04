997f9499e598fd4cf9872b86806c91b85ceff6f3
Andreea <amuresan@ualberta.ca>
2020-06-04 17:27:18 -0600

    linted

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,34 @@
-import React from 'react';
-import styles from '../styles/header.module.css';
+import React from "react";
+import styles from "../styles/header.module.css";
 
 const isCurrent = (link) => {
   return true;
   // return window.location.href.contains(link);
-}
+};
 
 const Header = () => {
   return (
     <div className={styles.container}>
-      <a href="#home" className={styles.logo} >Quizly</a>
+      <a href="#home" className={styles.logo}>
+        Quizly
+      </a>
       <div className={styles.containerRight}>
-        <a href="#mydecks" className={isCurrent("#mydecks") ? styles.currentpage : ""}>My Decks</a>
-        <a href="#account" className={isCurrent("#account") ? styles.currentpage : ""}>Account</a>
+        <a
+          href="#mydecks"
+          className={isCurrent("#mydecks") ? styles.currentpage : ""}
+        >
+          My Decks
+        </a>
+        <a
+          href="#account"
+          className={isCurrent("#account") ? styles.currentpage : ""}
+        >
+          Account
+        </a>
         <a href="#user">User</a>
       </div>
     </div>
   );
-}
+};
 
 export default Header;
